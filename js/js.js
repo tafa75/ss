61ea5958081fe21773392745189eeed5d3c275b3
tafa75 <63372691+tafa75@users.noreply.github.com>
2020-07-26 09:40:34 +0200

    Initial commit

---
+++ js/js.js
@@ -0,0 +1,23 @@
+let image = document.getElementById("baobab")
+image.onclick =(event) =>{
+    event.target.src ="img/case.jpeg";
+}
+
+
+
+
+
+
+let nombre = document.getElementById("nombre");
+nombre.value = ("mi nombre es");
+let apellido = document.getElementById("apellido");
+apellido.value =("mi nombre es");
+
+
+//apellidoElt.addEventListener("focus", function () {
+   //var apellido= document.getElementById("apellido").textContent = "apellido";
+//});
+// Suppression du message contextuel pour la saisie du pseudo
+//pseudoElt.addEventListener("blur", function (e) {
+    //document.getElementById("aidePseudo").textContent = "";
+//});
